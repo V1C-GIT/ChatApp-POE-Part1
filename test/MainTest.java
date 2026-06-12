04d8331ce524fe7a0c9b0c734037dc0a2cc77ab4
V1C-GIT <thabov78@gmail.com>
2026-06-12 17:35:42 +0200

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,54 +1,153 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
+package quickchatapp;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import quickchat.Main;
 
 /**
  *
  * @author Thabo
  */
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class MainTest 
-{
-
-    @Test
-    public void testValidUsername() 
-    {
-        assertTrue(Main.isValidUsername("user_"));
+public class MainTest {
+    
+    public MainTest() {
     }
 
+    /**
+     * Test of main method, of class Main.
+     */
     @Test
-    public void testInvalidUsername()
-    {
-        assertFalse(Main.isValidUsername("usr"));
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Main.main(args);
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of Userregistration method, of class Main.
+     */
     @Test
-    public void testValidPassword()
-    {
-        assertTrue(Main.isValidPassword("Password1!"));
+    public void testUserregistration() {
+        System.out.println("Userregistration");
+        Main.Userregistration();
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of Userlogin method, of class Main.
+     */
     @Test
-    public void testInvalidPassword()
-    {
-        assertFalse(Main.isValidPassword("password"));
+    public void testUserlogin() {
+        System.out.println("Userlogin");
+        Main.Userlogin();
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of startQuickChat method, of class Main.
+     */
     @Test
-    public void testValidPhone() 
-    {
-        assertTrue(Main.isValidPhone("+27831234567"));
+    public void testStartQuickChat() {
+        System.out.println("startQuickChat");
+        Main.startQuickChat();
+        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of sendMessage method, of class Main.
+     */
     @Test
-    public void testInvalidPhone() 
-    {
-        assertFalse(Main.isValidPhone("0831234567"));
+    public void testSendMessage() {
+        System.out.println("sendMessage");
+        int messageNumber = 0;
+        Main.sendMessage(messageNumber);
+        fail("The test case is a prototype.");
     }
+
+    /**
+     * Test of showSentMessages method, of class Main.
+     */
+    @Test
+    public void testShowSentMessages() {
+        System.out.println("showSentMessages");
+        Main.showSentMessages();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of storedMessagesMenu method, of class Main.
+     */
+    @Test
+    public void testStoredMessagesMenu() {
+        System.out.println("storedMessagesMenu");
+        Main.storedMessagesMenu();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displaySenderRecipient method, of class Main.
+     */
+    @Test
+    public void testDisplaySenderRecipient() {
+        System.out.println("displaySenderRecipient");
+        Main.displaySenderRecipient();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayLongestMessage method, of class Main.
+     */
+    @Test
+    public void testDisplayLongestMessage() {
+        System.out.println("displayLongestMessage");
+        Main.displayLongestMessage();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of searchByMessageID method, of class Main.
+     */
+    @Test
+    public void testSearchByMessageID() {
+        System.out.println("searchByMessageID");
+        Main.searchByMessageID();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of searchByRecipient method, of class Main.
+     */
+    @Test
+    public void testSearchByRecipient() {
+        System.out.println("searchByRecipient");
+        Main.searchByRecipient();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteByHash method, of class Main.
+     */
+    @Test
+    public void testDeleteByHash() {
+        System.out.println("deleteByHash");
+        Main.deleteByHash();
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayFullReport method, of class Main.
+     */
+    @Test
+    public void testDisplayFullReport() {
+        System.out.println("displayFullReport");
+        Main.displayFullReport();
+        fail("The test case is a prototype.");
+    }
+    
 }
+
